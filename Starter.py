93abff0bd8b072e3facf6b8377075d07d033ee83
pighax <71028333+oyzipfile@users.noreply.github.com>
2021-12-24 21:44:24 +0300

    init

---
+++ Starter.py
@@ -0,0 +1,49 @@
+import sys
+import time
+import requests
+from colorama import Fore, init
+
+init()
+print(f"""
+    {Fore.LIGHTBLUE_EX}███████╗███╗   ██╗████████╗██╗██████╗ ███████╗███████╗    ██████╗  ██████╗ ████████╗
+    ██╔════╝████╗  ██║╚══██╔══╝██║██╔══██╗██╔════╝██╔════╝    ██╔══██╗██╔═══██╗╚══██╔══╝
+    █████╗  ██╔██╗ ██║   ██║   ██║██████╔╝█████╗  █████╗█████╗██████╔╝██║   ██║   ██║   
+    ██╔══╝  ██║╚██╗██║   ██║   ██║██╔══██╗██╔══╝  ██╔══╝╚════╝██╔══██╗██║   ██║   ██║   
+    ███████╗██║ ╚████║   ██║   ██║██║  ██║███████╗███████╗    ██████╔╝╚██████╔╝   ██║   
+    ╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝    ╚═════╝  ╚═════╝    ╚═╝   
+         {Fore.RESET}https://github.com/Py-Pig/EntireeBot | Py-Pig: developed by PigHax                                                            
+""")
+
+prefix = f"[=] "
+
+
+def start():
+    token = input(f"{prefix}Token: ")
+    headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11", "content-type": "application/json", "Authorization": token}
+    if requests.get('https://discord.com/api/v9/users/@me', headers=headers).status_code != 200:
+        print(f"{prefix}Invalid token, exit.")
+        time.sleep(5)
+        sys.exit()
+        return
+
+    channeled = int(input(f"{prefix}Channel Id: "))
+    delay = float(input(f"{prefix}Delay (float value): "))
+    startValue = int(input(f"{prefix}Start value: "))
+    endValue = int(input(f"{prefix}End value: "))
+
+    i = startValue
+
+    while i < endValue:
+        time.sleep(delay)
+        message = requests.post(f"https://discord.com/api/v8/channels/{channeled}/messages", headers=headers,
+                                json={"content": str(f"{i}")})
+        if message.status_code == 200:
+            i = i + 1
+            print(f"{prefix}Message sended = [i=[{i}]] [{message.status_code}]")
+        else:
+            print(f"{prefix}Failed to send message = [i=[{i}]] [{message.status_code}]")
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    start()
