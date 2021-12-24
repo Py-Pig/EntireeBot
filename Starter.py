fbe9dc66c4b5e030eaf43125a96cc79b0f4fcffc
pighax <71028333+oyzipfile@users.noreply.github.com>
2021-12-25 00:09:26 +0300

    Json config system

--- Starter.py
+++ Starter.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import json
+
 import requests
 from colorama import Fore, init
 
@@ -15,27 +17,35 @@ print(f"""
 """)
 
 prefix = f"[=] "
-
-token = input(f"{prefix}Token: ")
-channeled = int(input(f"{prefix}Channel Id: "))
-delay = float(input(f"{prefix}Delay (float value): "))
-startValue = int(input(f"{prefix}Start value: "))
-endValue = int(input(f"{prefix}End value: "))
+cfg = open('config.json')
+cfg = json.load(cfg)
 
 
 def start():
+
+    print(f"""
+{prefix}current config:
+
+{prefix}Token: *********************
+{prefix}Channel id: {cfg["ChannelId"]}
+{prefix}Delay: {cfg["Delay"]}
+{prefix}Start Value: {cfg["StartValue"]}
+{prefix}End Value: {cfg["EndValue"]}
+    """)
+
     headers = {
         "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
-        "content-type": "application/json", "Authorization": token}
+        "content-type": "application/json", "Authorization": cfg['Token']}
     if requests.get('https://discord.com/api/v9/users/@me', headers=headers).status_code != 200:
         print(f"{prefix}Invalid token, exit.")
+        time.sleep(5)
         sys.exit()
         return
 
-    i = startValue
-    while i < endValue:
-        time.sleep(delay)
-        message = requests.post(f"https://discord.com/api/v8/channels/{channeled}/messages", headers=headers,
+    i = cfg['StartValue']
+    while i < cfg['EndValue']:
+        time.sleep(cfg['Delay'])
+        message = requests.post(f"https://discord.com/api/v8/channels/{cfg['ChannelId']}/messages", headers=headers,
                                 json={"content": str(f"{i}")})
         if message.status_code == 200:
             i = i + 1
